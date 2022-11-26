d939792899d2ccbaa11cb2fad62362d0f3b85978
Morel-D <morelDen@gmail.com>
2022-11-25 17:58:33 -0800

    Sign, password hash & token generetor

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,7 +1,27 @@
+const User = require('../Models/authModels');
+const jwt = require('jsonwebtoken');
 
 
-const signUpBlog = (req, res) => {
-    res.status(200).json({msg: "Signup is successful"})
+const createToken = (_id) =>
+{
+    return jwt.sign({_id}, process.env.SECRETE, { expiresIn: '3d' })
+}
+ 
+
+const signUpBlog = async (req, res) => {
+
+    const { userName, email, password } = req.body
+    
+    try {
+        const user = await User.signUp(userName, email, password);
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ userName, email, token })
+    } catch (error)
+    {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 const loginBlog = (req, res) => {
--- server/Models/authModels.js
+++ server/Models/authModels.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bycrypt = require('bcrypt');
+const { exists } = require('./blogModels');
 
 
 const SchemaUsers = new Schema(
     {
-        Username: {
+        userName: {
             type: String,
             required: true
         },
@@ -23,6 +25,37 @@ const SchemaUsers = new Schema(
     }, { timestamps: true }
 )
 
-const Users = mongoose.model('user', SchemaUsers);
 
+
+
+// Creating a function for signUp conditions
+SchemaUsers.statics.signUp = async function (userName, email, password)
+{
+
+    // create a variable which coantains an existing email 
+    const exist = await this.findOne({ email })
+
+    // Create condition in case this email already exist
+    if (exist) 
+    {
+        throw Error('This email already exxist')
+
+    }
+    
+    // In case it doesn't exist
+
+    // Password hasing
+    const salt = await bycrypt.genSalt(10);
+    const hash = await bycrypt.hash(password, salt);
+
+    // save them in a created variable
+    const user = await this.create({userName, email, password: hash })
+    
+    return user;
+
+    
+    }
+
+
+    const Users = mongoose.model('user', SchemaUsers);
 module.exports = Users
